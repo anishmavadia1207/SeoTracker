577b07508ea74e23a2ce222fc9eb0961fb8a997e
Anish Mavadia <anishmavadia1910@gmail.com>
2024-04-01 21:26:06 +0100

    Add additional failing tests for correct search time and invalid entries

---
+++ SeoTracker/src/SeoTracker.Api.Host/Models/SearchResult.cs
@@ -0,0 +1,7 @@
+﻿namespace SeoTracker.Api.Host.Models;
+
+public class SearchResult(string url, int position)
+{
+    public string Url { get; } = url;
+    public int Position { get; } = position;
+}
--- SeoTracker/tests/SeoTracker.Test.Integration/Controllers/SearchControllerShould.cs
+++ SeoTracker/tests/SeoTracker.Test.Integration/Controllers/SearchControllerShould.cs
@@ -1,7 +1,12 @@
 ﻿using System.Net;
+using System.Net.Http.Json;
+
+using Bogus;
 
 using FluentAssertions;
+using FluentAssertions.Execution;
 
+using SeoTracker.Api.Host.Models;
 using SeoTracker.Test.Integration.Configuration;
 
 namespace SeoTracker.Test.Integration.Controllers;
@@ -24,6 +29,23 @@ public class SearchControllerShould(SeoTrackerWebApplicationFactory factory)
     {
         var response = await _client.GetAsync($"/search/position?{searchTermQuery}&{urlSearchQuery}");
 
+        using var scope = new AssertionScope();
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+
+    [Fact]
+    public async Task Return_Positions_When_RequestParametersAreSupplied()
+    {
+        var faker = new Faker();
+        var url = faker.Internet.Url();
+        var searchTerm = faker.Lorem.Word();
+
+        var response = await _client.GetAsync($"/search/position?searchTerm={searchTerm}&url={url}");
+        var content = await response.Content.ReadFromJsonAsync<SearchResult>();
+
+        using var scope = new AssertionScope();
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        content.Should().NotBeNull();
+        content?.Url.Should().Be(url);
     }
 }
