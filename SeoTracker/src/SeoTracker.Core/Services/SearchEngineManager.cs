f57bc50ff2234fba9c7490a43335ed8e84094e65
Anish Mavadia <anishmavadia1910@gmail.com>
2024-04-02 07:56:16 +0100

    Add unimplemented manager

---
+++ SeoTracker/src/SeoTracker.Core/Services/SearchEngineManager.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+
+using SeoTracker.Abstractions.Models;
+using SeoTracker.Abstractions.Services;
+
+namespace SeoTracker.Core.Services;
+
+/// <summary>
+/// The implementation of <see cref="ISearchEngineManager"/>
+/// </summary>
+/// <param name="searchEngines">The search engines</param>
+/// <param name="logger">The logger</param>
+public class SearchEngineManager(
+    IEnumerable<ISearchEngineService> searchEngines,
+    ILogger<SearchEngineManager> logger) : ISearchEngineManager
+{
+    private readonly IEnumerable<ISearchEngineService> _searchEngineServices = searchEngines;
+    private readonly ILogger _logger = logger;
+
+    ///<inheritdoc/>
+    public Task<IEnumerable<SearchRankDto>> GetRanksAsync(
+        string searchTerm,
+        string url) => throw new NotImplementedException();
+}
