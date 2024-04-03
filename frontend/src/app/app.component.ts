0ccb872516310358bc445bbfbd85923b386ab882
Anish Mavadia <anishmavadia1910@gmail.com>
2024-04-03 09:03:26 +0100

    Update styles once mroe

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SearchFormComponent } from './search-form/search-form.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SearchFormComponent],
+  imports: [
+    RouterOutlet,
+    SearchFormComponent,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
