3c4a2eba992313dbf7159caeb0b90c7277fa87ec
Anish Mavadia <anishmavadia1910@gmail.com>
2024-04-03 07:26:36 +0100

    set mozilla web header

--- SeoTracker/src/SeoTracker.Api.Host/Program.cs
+++ SeoTracker/src/SeoTracker.Api.Host/Program.cs
@@ -22,6 +22,9 @@ builder.Services.AddHttpClient(
     client =>
     {
         client.BaseAddress = new Uri("https://www.google.com");
+        client.DefaultRequestHeaders.Add(
+            "User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36");
     });
 
 builder.Services.AddScoped<ISearchEngineService, GoogleSearchEngineService>();
